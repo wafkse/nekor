cd87554e7e01dd71a42e5494b88f76b75ebf488f
W. Frakchi <noreply@wafkse.dev>
2026-01-31 03:54:30 +0000

    nekor-hal: add hardware abstraction layer skeleton

---
+++ nekor-hal/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![forbid(
+    clippy::all,
+    clippy::perf,
+    clippy::nursery,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::pedantic,
+    rustdoc::all
+)]
+//! A Hardware Abstraction Layer (HAL) for the Nekor unikernel.
