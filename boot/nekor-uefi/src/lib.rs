cdb303d9fdb95a4d06adbf9f17ab3944872f365d
W. Frakchi <noreply@wafkse.dev>
2026-02-16 19:03:26 +0000

    nekor-uefi: add UEFI boot skeleton

---
+++ boot/nekor-uefi/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![forbid(
+    clippy::all,
+    clippy::perf,
+    clippy::nursery,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::pedantic,
+    rustdoc::all
+)]
+//! Bootloader implementation for the Nekor Boot Protocol for UEFI systems.
