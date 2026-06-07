ce94471c708e2ae0833263f574686f40de66e45d
W. Frakchi <noreply@wafkse.dev>
2026-06-07 22:59:05 +0000

    nekor-platform: add platform build script and config generation

---
+++ nekor-platform/build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // TODO: Add platform ENV variables and make it add all tera and generated
+    // build files to dependency tree.
+}
---
+++ nekor-platform/src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![forbid(
+    clippy::all,
+    clippy::perf,
+    clippy::nursery,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::pedantic,
+    rustdoc::all
+)]
+//! A crate to access various platform information for the kernel.
+
+// TODO: Make the platform files inject environment variables for metadata.
